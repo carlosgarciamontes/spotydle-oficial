2845a09efb3c3a8e7fa0e3450e7a053457bf2d37
carlosgarciamontes <carlosgarciamontes@elpuig.xeill.net>
2026-05-17 16:22:59 +0200

    más artistas

--- src/data/playlists.ts
+++ src/data/playlists.ts
@@ -14,6 +14,44 @@ export const PLAYLISTS: Record<string, string[]> = {
     "1548066850", // dvalentino
     "1512674580", // xiyo
     "1174817313", // bad gyal
+    "1715924692", // guxo
+    "1630709726", // 8belial
+    "1548709945", // cmari
+    "1545910118", // tarchi
+    "264654132", // metrika
+    "1641363362", // l0rna
+    "1712188003", // biberon
+    "1535491772", // gloosito
+    "1460130646", // leiti
+    "1389882141", // bon calso
+    "1473762560", // mda
+    "1479887593", // john pollon
+    "1488363414", // ralphie
+    "937546425", // sticky
+    "1060693799", // enry-k
+    "1470128639", // jay dime
+    "1625125080", // superreservao
+    "1388592825", // rusowsky
+    "1393108284", // juicy
+    "1579913692", // ynestrosa
+    "1664931491", // roomtrash
+    "1462267982", // l'haine
+    "1711768744", // ebano
+    "1487503245", // judeline
+    "1447182168", // ill pekeño
+    "1174698354", // ergo pro
+    "1585100986", // bbdemon
+    "1578112294", // mushka
+
+    // --- 90s ---
+    "32940", // mj
+    "112018", // nirvana
+    "20044", // madonna
+    "106158", // cher
+    "259398", // christina aguilera
+    "17360", // tlc
+    "27373453", // mecano
+
     // --- REGGAETON ---
     "1126808565", // badbunny
     "25514958", // Daddy Yankee
@@ -24,16 +62,73 @@ export const PLAYLISTS: Record<string, string[]> = {
     "1210225867", // myke towers
     "76538401", // cosculluela
     "80993837", // wisin y yandel
+    "448916501", // maluma
+    "444520760", // jbalvin
+    "381641802", // plan b
+    "194502100", // feid
+    "193167653", // tainy
+    "28215216", // arcangel
+    "76538118", // tego
+    "33987470", // nicky jam
+
+    // --- HIPHOP ---
+    "112058", // 50cent
+    "35307", // nas
+    "2715720", // kanye
+    "128050210", // future
+    "1352449404", // jayz
+    "5499810", // notorius
+    "105998", // 2pac
+    "549236696", // travis
+    "516663045", // sosa
+    "940710524", // uzi
+    "1276656483", // lil baby
+    "982372505", // playboi carti
+    "368183298", // kendrick
+    "605800394", // sza
+    "420368335", // tyler
+    "1203172438", // trippie redd
+    "81886939", // young thug
+    "488479277", // rich gang
+    "1450601383", // pop smoke
+    "21769", // snoop dogg
+    "35315", // drdre
+    "77135", // mobb deep
+
+    // --- FLOW-2000 ---
+    "459885", // avril
+    "148662", // linkin
+    "217005", // britney
+    "634287", // destiny's child
+    "1419227", // beyonce
+    "3261325", // akon
+    "63346553", // rihanna
+    "277293880", // lady gaga
+    "360391", // black eyed peas
+    "64387566", // katy perry
+
+    // --- ROCK ---
+    "3296287", // queen
+    "5040714", // acdc
+    "106621", // guns n roses
+    "3996865", // metallica
+    "112018", // nirvana
+    "311145", // rem
+    "166529", // van halen
+    "602212", // scorpions
+    "122782", // bon jovi
+    "115386", // aerosmith
+    "1249595", // roling stones
+    "462614", // toto
+    "165907", // black sabath
+
     // --- POP ESP ---
     "1342086366", // Enol
     "1444166672", // Funzo & Baby Loud
     "376461725", // P.Alboran
     "14508321", // La Oreja
-    // --- OTROS ---
-    "32940", "112018", // 90s
-    "112058", "35307", // hiphop
-    "459885", "148662", // flow-2000
-    "3296287", "5040714", // rock
+    "1455990127", // walls
+    "2307408", // amaral
   ],
   urbano: [
     "332979733", // hoke
@@ -48,8 +143,44 @@ export const PLAYLISTS: Record<string, string[]> = {
     "1548066850", // dvalentino
     "1512674580", // xiyo
     "1174817313", // bad gyal
+    "1715924692", // guxo
+    "1630709726", // 8belial
+    "1548709945", // cmari
+    "1545910118", // tarchi
+    "264654132", // metrika
+    "1641363362", // l0rna
+    "1712188003", // biberon
+    "1535491772", // gloosito
+    "1460130646", // leiti
+    "1389882141", // bon calso
+    "1473762560", // mda
+    "1479887593", // john pollon
+    "1488363414", // ralphie
+    "937546425", // sticky
+    "1060693799", // enry-k
+    "1470128639", // jay dime
+    "1625125080", // superreservao
+    "1388592825", // rusowsky
+    "1393108284", // juicy
+    "1579913692", // ynestrosa
+    "1664931491", // roomtrash
+    "1462267982", // l'haine
+    "1711768744", // ebano
+    "1487503245", // judeline
+    "1447182168", // ill pekeño
+    "1174698354", // ergo pro
+    "1585100986", // bbdemon
+    "1578112294", // mushka
   ],
-  "90s": ["32940", "112018"],
+  "90s": [
+    "32940", // mj
+    "112018", // nirvana
+    "20044", // madonna
+    "106158", // cher
+    "259398", // christina aguilera
+    "17360", // tlc
+    "27373453", // mecano
+  ],
   reggaeton: [
     "1126808565", // badbunny
     "25514958", // Daddy Yankee
@@ -60,14 +191,72 @@ export const PLAYLISTS: Record<string, string[]> = {
     "1210225867", // myke towers
     "76538401", // cosculluela
     "80993837", // wisin y yandel
+    "448916501", // maluma
+    "444520760", // jbalvin
+    "381641802", // plan b
+    "194502100", // feid
+    "193167653", // tainy
+    "28215216", // arcangel
+    "76538118", // tego
+    "33987470", // nicky jam
   ],
-  hiphop: ["112058", "35307"],
-  "flow-2000": ["459885", "148662"],
-  rock: ["3296287", "5040714"],
+  hiphop: [
+    "112058", // 50cent
+    "35307", // nas
+    "2715720", // kanye
+    "128050210", // future
+    "1352449404", // jayz
+    "5499810", // notorius
+    "105998", // 2pac
+    "549236696", // travis
+    "516663045", // sosa
+    "940710524", // uzi
+    "1276656483", // lil baby
+    "982372505", // playboi carti
+    "368183298", // kendrick
+    "605800394", // sza
+    "420368335", // tyler
+    "1203172438", // trippie redd
+    "81886939", // young thug
+    "488479277", // rich gang
+    "1450601383", // pop smoke
+    "21769", // snoop dogg
+    "35315", // drdre
+    "77135", // mobb deep
+  ],
+  "flow-2000": [
+    "459885", // avril
+    "148662", // linkin
+    "217005", // britney
+    "634287", // destiny's child
+    "1419227", // beyonce
+    "3261325", // akon
+    "63346553", // rihanna
+    "277293880", // lady gaga
+    "360391", // black eyed peas
+    "64387566", // katy perry
+  ],
+  rock: [
+    "3296287", // queen
+    "5040714", // acdc
+    "106621", // guns n roses
+    "3996865", // metallica
+    "112018", // nirvana
+    "311145", // rem
+    "166529", // van halen
+    "602212", // scorpions
+    "122782", // bon jovi
+    "115386", // aerosmith
+    "1249595", // roling stones
+    "462614", // toto
+    "165907", // black sabath
+  ],
   "pop-esp": [
     "1342086366", // Enol
     "1444166672", // Funzo & Baby Loud
     "376461725", // P.Alboran
     "14508321", // La Oreja
+    "1455990127", // walls
+    "2307408", // amaral
   ],
 };
